246d9d3a6c38944de56e25daefcff1f1fa376877
Sian Richards <martin@MARTINs-MBP-2.default>
2019-06-11 15:59:04 +0100

    home page changes made

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import styles from "./home.module.scss";
 import Header from "../components/header";
 import Footer from "../components/footer"
+import Jonah from "../static/images/jonah.jpg";
 
 export interface IProps {
     
@@ -15,10 +16,17 @@ class Home extends React.Component<IProps, IState> {
     // state = { :  }
     render() { 
         return ( <React.Fragment> <Header /><article className={styles.home}>
-            <h2>Home page</h2>
-            <h3>Available Now</h3>
+        <div className={styles.imagesection}>
+        <h2>A futuristic, humorous retelling of the book of Jonah. </h2>
+                    <img src={Jonah} />
+                    </div>
+        <div className={styles.homeinfo}>
+            <h2>Available Now</h2>
             <button>Order Here</button>
+            <h3> Jo Sheringham writes stories which allow the extraordinary to stand out from the ordinary whilst celebrating both. </h3>
+            
             <button>Learn More</button>
+            </div>
         </article> 
         <Footer />
         </React.Fragment>);
