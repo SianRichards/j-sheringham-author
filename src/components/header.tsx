77f6e4b24a1020314896b2fc06011c606b57d62c
Sian Richards <martin@MARTINs-MacBook-Pro-2.local>
2019-06-20 14:47:48 +0100

    font awesome icons added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import styles from "./header.module.scss";
 import Nav from "./nav";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export interface IProps {
     
@@ -14,9 +17,15 @@ class Header extends React.Component<IProps, IState> {
     // state = { :  }
     render() { 
         return ( <React.Fragment><header className={styles.header}>
+        <div className={styles.links}>
+        <a href="https://www.facebook.com/sheringhambooks/Facebook" target="_blank"><FontAwesomeIcon icon={faFacebook} className={styles.icons} /></a>
+            <a href="mailto:jo@sheringhambooks.com"><FontAwesomeIcon icon={faEnvelope} className={styles.icons} /></a>
+            </div>
+            <div >
             <h1 className={styles.title}>
             Jo Sheringham
             </h1>
+            </div>
         </header> 
         <Nav />
         </React.Fragment>);
