8c2f621a1dfa52bbc3acf96465cf6a536617cc4d
Sian Richards <sian_richards@hotmail.co.uk>
2023-10-11 22:30:12 +0100

    page not found changes

--- src/pages/page-not-found/index.tsx
+++ src/pages/page-not-found/index.tsx
@@ -6,7 +6,7 @@ const PageNotFound = () => {
     <article className={styles.main}>
       <div className={styles.text}>
         <h1>Page Not Found</h1>
-        <h2>We're unable to find the page you're looking for.</h2>
+        <h3>We're unable to find the page you're looking for.</h3>
         <h4>
           Please use the navigation bar above to help get you back on track.
         </h4>
